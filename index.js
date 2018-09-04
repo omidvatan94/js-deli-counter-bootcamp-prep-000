b0fee6d00825da34fe25b9205091ce8d05c941d3
Omid Vatandoost <omidvatan94@gmail.com>
2018-09-04 20:40:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function currentLine(KatzDeliLine) {
   } else {
     var x = "The line is currently: "
     for(var i = 0; i < KatzDeliLine.length; i++) {
-      x +=  (i+1) + ". " + KatzDeliLine[i] 
+      x += (i+1) + ". " + KatzDeliLine[i] 
       if(i !== KatzDeliLine.length-1) { 
         x += ", "
       }
